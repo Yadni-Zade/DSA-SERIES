4e36b60fd936753bfd45a6161e929aa3caffc6fc
Your Name <zadeyadni9@gmail.com>
2026-06-21 11:30:19 +0530

    operators

--- variables_operators.cpp
+++ variables_operators.cpp
@@ -2,6 +2,48 @@
 using namespace std;
 int main()
 {
-    cout << "yadni \n zade";
-    return 0;
+   // cout << "yadni \n zade";
+   char grade = 'A';
+   int value = grade ;
+   cout<<value<<endl;
+   double price = 100.99;
+   int new_price = (int)price;
+    cout<<new_price<<endl;
+    
+ //operators 
+ //arithmetic 
+ int a , b , sum , sub , mul , div , mod ;
+ a=10;
+ b=5;
+ sum = a + b;
+ sub = a - b;
+ mul = a * b;
+ div = a / b;
+ mod = a % b;
+cout<<"sum = "<<sum<<endl;
+cout<<"sub = "<<sub<<endl;
+cout<<"mul = "<<mul<<endl;
+cout<<"div = "<<div<<endl;
+cout<<"mod = "<<mod<<endl;
+
+//relational 
+cout<<(10>5)<<endl;
+cout<<(10<5)<<endl;
+cout<<(10>=5)<<endl;
+cout<<(10<=5)<<endl;
+cout<<(10==5)<<endl;
+cout<<(10!=5)<<endl;
+
+
+// logical 
+
+cout<<(10>5 && 10<5)<<endl;
+cout<<(10>5 || 10<5)<<endl;
+cout<<!(10>5)<<endl;
+//unary 
+cout<<a++<<endl;
+cout<<++a<<endl;
+cout<<a--<<endl;
+cout<<--a<<endl;
+return 0;
 }
